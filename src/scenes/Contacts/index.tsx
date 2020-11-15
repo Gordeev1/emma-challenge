1d27cd185677ab7769c9b1156e843d0426a29e59
Artem Gordeev <gordeevartem.dev@gmail.com>
2020-11-15 17:19:16 +0300

    feat(Contacts): setup contacts screen

--- src/scenes/Contacts/index.tsx
+++ src/scenes/Contacts/index.tsx
@@ -1,5 +1,31 @@
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
+import ContactsLinkedSnapLists from '@modules/Contacts/ContactsLinkedSnapLists';
+import ContactsService from '@services/contacts';
+import { IContact } from '@LTypes/contact';
+import { handleError, showErrorMessage } from '@utils/error';
 
 export default () => {
-	return null;
+	// TODO: handle loading state
+	// eslint-disable-next-line @typescript-eslint/no-unused-vars
+	const [loading, setLoading] = useState(false);
+	const [data, setData] = useState<IContact[]>([]);
+
+	const load = useCallback(async () => {
+		setLoading(true);
+		try {
+			const results = await ContactsService.getContacts();
+			setData(results);
+		} catch (error) {
+			handleError(error);
+			showErrorMessage({ message: 'Failed to load contacts', error, onRetryPress: load });
+		} finally {
+			setLoading(false);
+		}
+	}, [setLoading, setData]);
+
+	useEffect(() => {
+		load();
+	}, [load]);
+
+	return <ContactsLinkedSnapLists data={data} />;
 };
